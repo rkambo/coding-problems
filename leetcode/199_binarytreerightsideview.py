532187a5daa286c139dc3214f72ec737ab30a9d5
Ravnoor Kambo <rkambo@uoguelph.ca>
2023-09-16 12:48:41 -0400

    Actual Leetcode 199

--- leetcode/199_binarytreerightsideview.py
+++ leetcode/199_binarytreerightsideview.py
@@ -1,30 +1,17 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        return []
-    def bfs(self, root):
-        queue = [ root ]
-        ret = []
-        while len(queue) > 0:
-            
-sol = Solution()
-
-L1 = TreeNode(1)
-L2 = TreeNode(2)
-L3 = TreeNode(3)
-L4 = TreeNode(4)
-L5 = TreeNode(5)
-
-L1.left = L2
-L1.right = L3
-L3.left = L4
-L2.right = L5
-
-print(sol.bfs(L1))
+def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    queue = [root]
+    ret = []
+    while queue:
+        rightSide = None
+        qLen = len(queue)
+        for i in range(qLen):
+            current = queue.pop(0)
+            if current:
+                rightSide = current
+                queue.append(current.left)
+                queue.append(current.right)
+        if rightSide:
+            ret.append(rightSide.val)
+    return ret
         
             
